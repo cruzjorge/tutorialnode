aff21cd76a4c7cfb8e9d80abaae54e6aa466e578
Jorge Cruz <cruzjorge.mx@gmail.com>
2016-02-17 23:56:17 -0600

    Siguendo curso hasta vdeo 17

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,6 +1,18 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/fotos");
+
+var userSchemaJSON = {
+    email:String,
+    password:String
+};
+
+var user_schema = new mongoose.Schema(userSchemaJSON);
+
+var User = mongoose.model("User", user_schema);
 
 app.use("/public", express.static('public'));
 
@@ -14,12 +26,19 @@ app.get("/", function(req, res) {
 });
 
 app.get("/login", function(req, res) {
-    res.render("login");
+    User.find(function(err,doc) {
+        console.log(doc);
+        res.render("login");
+    });
 });
 
 app.post("/users", function(req, res) {
-    console.log("Contraseña: " + req.body.password);
-    console.log("Email: " + req.body.email);
+    var user = new User ({email: req.body.email, password: req.body.password});
+
+    user.save(function () {
+        res.send("Guardamos tus datos");
+    });
+
     res.send("Recibimos tus datos");
 });
 
